c2484425ee257f44ecc3ed193d7fe20a46ab5f80
gardhi <gard.hillestad@gmail.com>
2015-11-12 18:33:22 +0100

    Finds all correct param. (not M)
    does not simulate properly

--- oppg/Files 2015/Estimation_script.m
+++ oppg/Files 2015/Estimation_script.m
@@ -1,8 +1,12 @@
+
+format
 % SysID Computer Exercise
 
 h = 1730; %m; h = h_d = h_a
 M = 10e9; %kg/m^4
 g = 9.8;
+
+time_span = (length(LOG3.t)-1)/60
 
 %% finding rho_a
 
@@ -20,7 +24,7 @@ line([0 length(LOG3.t)],[avg_p_hdiff avg_p_hdiff])
 legend('p_{dh}','p_c','p_{dh}-p_c')
 
 % parameter attained
-rho_a = (1/g*h)*avg_p_hdiff;
+rho_a = (1/(g*h))*avg_p_hdiff;
 
 
 %% Steady state analysis LOG1
@@ -92,112 +96,219 @@ legend('z_c','q_c','q_p','q_{bpp}','p_p','p_c')
 
 
 
-%% Attempt at finding C_a, D_d and rho_d :: q_p = q_c = q
-
-ss_interval_1 = 760:930;
-ss_interval_2 = 1350:1500;
-% 1st interval data point matching
-p_dh1_int1 = LOG1.p_dh(ss_interval_1);
-unNaNed_p_dh1_int1 = p_dh1_int1(~isnan(p_dh1_int1));
-unNaNed_p_c1_int1 = LOG1.p_c(ss_interval_1);
-unNaNed_p_c1_int1 = unNaNed_p_c1_int1(~isnan(p_dh1_int1));
-% 2nd interval data point matching
-p_dh1_int2 = LOG1.p_dh(ss_interval_2);
-unNaNed_p_dh1_int2 = p_dh1_int2(~isnan(p_dh1_int2));
-unNaNed_p_c1_int2 = LOG1.p_c(ss_interval_2);
-unNaNed_p_c1_int2 = unNaNed_p_c1_int2(~isnan(p_dh1_int2));
-% time points
-unNaNed_t1_int1 = ss_interval_1(~isnan(p_dh1_int1));
-unNaNed_t1_int2 = ss_interval_2(~isnan(p_dh1_int2));
-
-avg_q_c_int1 = sum(LOG1.q_c(ss_interval_1)) / length(LOG1.q_c(ss_interval_1));
-avg_q_c_int2 = sum(LOG1.q_c(ss_interval_2)) / length(LOG1.q_c(ss_interval_2));
 
 
-C_a1 = (1/avg_q_c_int1)*(unNaNed_p_c1_int1 - unNaNed_p_dh1_int1 + rho_a*g*h);
-C_a2 = (1/avg_q_c_int2)*(unNaNed_p_c1_int2 - unNaNed_p_dh1_int2 + rho_a*g*h);
+%% Steady State Curve fitting version 2
 
-C_a1 = sum(C_a1)/length(C_a1);
-C_a2 = sum(C_a2)/length(C_a2);
+t1 = 1;
+t2 = length(LOG2.t);
+t = LOG2.t(t1:t2);
 
-C_a = (C_a1 + C_a2) / 2;
+% Moving average smooths out graphs
+a = 10;
+B = 1/a*ones(a,1);
+q = filter(B,1,LOG2.q_c(t1:t2));
+p_c = filter(B,1,LOG2.p_c(t1:t2));
+p_p = filter(B,1,LOG2.p_p(t1:t2));
 
-%% Steady state curve fitting
-% For finding C_a, D_d and rho_d
+figure(3); clf(3)
+plot(t, q); hold on
+plot(t, p_p)
+plot(t, p_c)
 
-q_set = zeros(400,200); % wont need more than one q pr every second point
-p_c_set = zeros(400,200);
-p_p_set = zeros(400,200);
-avg_q_set = zeros(1,length(LOG2.t/2));
-m = 1;
-n = 1;
-set_ongoing = false;
 
-for i = 11:(length(LOG2.t)-10);
-    
-    if(( abs(LOG2.q_c(i-7)-LOG2.q_c(i)) < 10 ) ...         % checking k1 samples to the left
-            && ( abs(LOG2.q_c(i+7)-LOG2.q_c(i)) < 10 ) )   % checking k2 samples to the right
-        q_set(n,m) = LOG2.q_c(i);                          % if not too different assume steady state
-        p_c_set(n,m) = LOG2.p_c(i);
-        p_p_set(n,m) = LOG2.p_p(i);
-        n = n + 1;
-        set_ongoing = true;
-    else
-        if set_ongoing
-            m = m+1;
-            n = 1;
-            set_ongoing = false;
-        end
-        
-    end
+% finding saddle points
+derivatives = zeros(1,t2);
+for i = a:t2-1
+    derivatives(i) = q(i+1)-q(i);
 end
+tollerance = 0.1;
+saddle_points = find(abs(derivatives) < tollerance);
+t_ss = t(saddle_points);
+t_ss = t_ss(10:end);
+q_ss = q(saddle_points);
+q_ss = q_ss(10:end);
+p_c_ss = p_c(saddle_points);
+p_c_ss = p_c_ss(10:end);
+p_p_ss = p_p(saddle_points);
+p_p_ss = p_p_ss(10:end);
 
-measurments_for_use = [];
-k = 1;
-for i = 1: length(q_set(1,:))
-    temp = length(find(q_set(:,i)));
-    if temp > 5;
-        measurments_for_use(k) = i;
-        k = k+1;
-    end
-end
+plot(t_ss, q_ss, 'ored')
 
-avg_q_measurments = zeros(1,length(measurments_for_use));
-avg_p_c_measurments = zeros(1,length(measurments_for_use));
-avg_p_p_measurments = zeros(1,length(measurments_for_use));
+figure(4);clf(4)
+plot(q_ss, p_p_ss - p_c_ss - (rho_a*g*h))
+hold on
+poly = polyfit(q_ss, p_p_ss - p_c_ss - (rho_a*g*h),2);
+plot(q_ss, poly(1)*q_ss.^2 + poly(2)*q_ss + poly(3),'ored')
 
-for i = 1:length(measurments_for_use)
-    this_q_set = q_set(find(q_set(:,measurments_for_use(i))), measurments_for_use(i));
-    avg_q_measurments(i) = sum(this_q_set)/length(this_q_set);
-    this_p_c_set = p_c_set(find(p_c_set(:,measurments_for_use(i))), measurments_for_use(i));
-    avg_p_c_measurments(i) = sum(this_p_c_set)/length(this_p_c_set);
-    this_p_p_set = p_p_set(find(p_p_set(:,measurments_for_use(i))), measurments_for_use(i));
-    avg_p_p_measurments(i) = sum(this_p_p_set)/length(this_p_p_set);
-end   
-
-figure(11); clf(11);
-plot(avg_q_measurments ,  - (rho_a*g*h) - avg_p_c_measurments + avg_p_p_measurments)
-hold on;
-poly = polyfit(avg_q_measurments , - (rho_a*g*h) - avg_p_c_measurments + avg_p_p_measurments,2);
-plot(avg_q_measurments, poly(1)*avg_q_measurments.^2 + poly(2)*avg_q_measurments + poly(3), 'o')
-hold off;
-
-rho_d = poly(3)/(g*h)
+rho_d = -poly(3)/(g*h)
 C_a = poly(2)
 D_d = poly(1)
 
 
-%%
+%% Estimating g_c
 LOG = LOG3;
-figure(11); clf(11)
-plot(LOG.t, LOG.p_c- LOG.p_c_ds)
 
-g_c = LOG.q_c ./ sqrt(LOG.p_c -LOG.p_c_ds);
+figure(10); clf(10)
+plot(LOG.t, LOG.p_c-LOG.p_c_ds)
+t1 = 500; t2 = 5000;
+
+g_c = LOG.q_c(t1:t2) ./ sqrt(LOG.p_c(t1:t2) -LOG.p_c_ds(t1:t2));
 
 figure(12);clf(12)
-plot(LOG.t, g_c)
+plot(LOG.z_c(t1:t2), g_c)
+hold on
+
+theta = polyfit(LOG.z_c(t1:t2), g_c, 2);
+plot(LOG.z_c(t1:t2), theta(1)*LOG.z_c(t1:t2).^2 + theta(2)*LOG.z_c(t1:t2) +theta(3), 'o')
+
+%% Estimating Va/Beta_a version 2
+
+t1 = 800; t2 = 1800;
+
+LOG = LOG3;
+p_c = LOG.p_c(t1:t2);
+q_bpp = LOG.q_bpp(t1:t2);
+q_c = LOG.q_c(t1:t2);
+t = LOG.t(t1:t2);
+
+dp_c_dt = diff(p_c);
+cut = 200;
+
+figure(3); clf(3)
+plot(dp_c_dt(1:end-cut+1), q_bpp(1:end-cut) - q_c(1:end-cut),'.'); hold on
+C1_poly = polyfit(dp_c_dt(1:end-cut+1), q_bpp(1:end-cut) - q_c(1:end-cut),1);
+plot(dp_c_dt(1:end-cut+1), C1_poly(1)*dp_c_dt(1:end-cut+1)+ C1_poly(2), 'ored')
+
+C1 = C1_poly(1);
 
 
+%% Estimating V_d/Beta_d version 2
+
+LOG = LOG1;
+% LOG2 intervals
+%t1 = 500; t2 = 3000;
+% LOG1 intervals
+t1 = 400; t2 = 600;
+
+p_c = LOG.p_c(t1:t2); 
+p_p = LOG.p_p(t1:t2);
+q_bpp = LOG.q_bpp(t1:t2);
+q_c = LOG.q_c(t1:t2);
+q_p = LOG.q_p(t1:t2);
+t = LOG.t(t1:t2);
+
+dp_p_dt = diff(p_p);
+dp_c_dt = diff(p_c);
+
+C2_poly = polyfit(dp_p_dt, q_p(1:end-1)-(C1*dp_c_dt)+q_bpp(1:end-1)...
+    -q_c(1:end-1),1);
+figure(5); clf(5)
+plot(dp_p_dt, q_p(1:end-1)-(C1*dp_c_dt)+q_bpp(1:end-1)- q_c(1:end-1),'.');
+hold on
+plot(dp_p_dt, C2_poly(1)*dp_p_dt + C2_poly(2), 'ored')
+
+C2 = C2_poly(1);
+%%
+
+
+
+% 
+% %% Attempt at finding C_a, D_d and rho_d :: q_p = q_c = q
+% % Wrong attempt
+% 
+% ss_interval_1 = 760:930;
+% ss_interval_2 = 1350:1500;
+% % 1st interval data point matching
+% p_dh1_int1 = LOG1.p_dh(ss_interval_1);
+% unNaNed_p_dh1_int1 = p_dh1_int1(~isnan(p_dh1_int1));
+% unNaNed_p_c1_int1 = LOG1.p_c(ss_interval_1);
+% unNaNed_p_c1_int1 = unNaNed_p_c1_int1(~isnan(p_dh1_int1));
+% % 2nd interval data point matching
+% p_dh1_int2 = LOG1.p_dh(ss_interval_2);
+% unNaNed_p_dh1_int2 = p_dh1_int2(~isnan(p_dh1_int2));
+% unNaNed_p_c1_int2 = LOG1.p_c(ss_interval_2);
+% unNaNed_p_c1_int2 = unNaNed_p_c1_int2(~isnan(p_dh1_int2));
+% % time points
+% unNaNed_t1_int1 = ss_interval_1(~isnan(p_dh1_int1));
+% unNaNed_t1_int2 = ss_interval_2(~isnan(p_dh1_int2));
+% 
+% avg_q_c_int1 = sum(LOG1.q_c(ss_interval_1)) / length(LOG1.q_c(ss_interval_1));
+% avg_q_c_int2 = sum(LOG1.q_c(ss_interval_2)) / length(LOG1.q_c(ss_interval_2));
+% 
+% 
+% C_a1 = (1/avg_q_c_int1)*(unNaNed_p_c1_int1 - unNaNed_p_dh1_int1 + rho_a*g*h);
+% C_a2 = (1/avg_q_c_int2)*(unNaNed_p_c1_int2 - unNaNed_p_dh1_int2 + rho_a*g*h);
+% 
+% C_a1 = sum(C_a1)/length(C_a1);
+% C_a2 = sum(C_a2)/length(C_a2);
+% 
+% C_a = (C_a1 + C_a2) / 2;
+% 
+% %% Steady state curve fitting version 1.
+% % For finding C_a, D_d and rho_d
+% 
+% q_set = zeros(400,200); % wont need more than one q pr every second point
+% p_c_set = zeros(400,200);
+% p_p_set = zeros(400,200);
+% avg_q_set = zeros(1,length(LOG2.t/2));
+% m = 1;
+% n = 1;
+% set_ongoing = false;
+% 
+% for i = 10:(length(LOG2.t)-10);
+%     
+%     if(( abs(LOG2.q_c(i-7)-LOG2.q_c(i)) < 10 ) ...         % checking k1 samples to the left
+%             && ( abs(LOG2.q_c(i+7)-LOG2.q_c(i)) < 10 ) )   % checking k2 samples to the right
+%         q_set(n,m) = LOG2.q_c(i);                          % if not too different assume steady state
+%         p_c_set(n,m) = LOG2.p_c(i);
+%         p_p_set(n,m) = LOG2.p_p(i);
+%         n = n + 1;
+%         set_ongoing = true;
+%     else
+%         if set_ongoing
+%             m = m+1;
+%             n = 1;
+%             set_ongoing = false;
+%         end
+%         
+%     end
+% end
+% 
+% measurments_for_use = [];
+% k = 1;
+% for i = 1: length(q_set(1,:))
+%     temp = length(find(q_set(:,i)));
+%     if temp > 5;
+%         measurments_for_use(k) = i;
+%         k = k+1;
+%     end
+% end
+% 
+% avg_q_measurments = zeros(1,length(measurments_for_use));
+% avg_p_c_measurments = zeros(1,length(measurments_for_use));
+% avg_p_p_measurments = zeros(1,length(measurments_for_use));
+% 
+% for i = 1:length(measurments_for_use)
+%     this_q_set = q_set(find(q_set(:,measurments_for_use(i))), measurments_for_use(i));
+%     avg_q_measurments(i) = sum(this_q_set)/length(this_q_set);
+%     this_p_c_set = p_c_set(find(p_c_set(:,measurments_for_use(i))), measurments_for_use(i));
+%     avg_p_c_measurments(i) = sum(this_p_c_set)/length(this_p_c_set);
+%     this_p_p_set = p_p_set(find(p_p_set(:,measurments_for_use(i))), measurments_for_use(i));
+%     avg_p_p_measurments(i) = sum(this_p_p_set)/length(this_p_p_set);
+% end   
+% 
+% figure(11); clf(11);
+% plot(avg_q_measurments ,  - (rho_a*g*h) - avg_p_c_measurments + avg_p_p_measurments)
+% hold on;
+% poly = polyfit(avg_q_measurments , - (rho_a*g*h) - avg_p_c_measurments + avg_p_p_measurments,2);
+% plot(avg_q_measurments, poly(1)*avg_q_measurments.^2 + poly(2)*avg_q_measurments + poly(3), 'o')
+% hold off;
+% % 
+% % rho_d = poly(3)/(g*h)
+% % C_a = poly(2)
+% % D_d = poly(1)
+% 
 
 
 %%
@@ -249,6 +360,46 @@ plot(LOG.t, g_c)
 % 
 % % conclusion LOG3 z_c =~ u_c
 
+
+%%
+% %% Estimating Va/Beta_a version 1
+% 
+% t1 = 800; t2 = 1800;
+% 
+% LOG = LOG3;
+% p_c = LOG.p_c(t1:t2);
+% q_bpp = LOG.q_bpp(t1:t2);
+% q_c = LOG.q_c(t1:t2);
+% t = LOG.t(t1:t2);
+% 
+% figure(4);clf(4)
+% q_bpp = smooth(q_bpp,40);
+% q_c = smooth(q_c,1000);
+% p_c = smooth(p_c,500);
+% 
+% plot(t,q_bpp); hold on
+% plot(t,q_c)
+% plot(t,p_c)
+% 
+% dp_c_dt = diff(p_c);
+% % smoothing of derivative
+% %  figure(5); clf(5)
+% %  plot(t(1:end-1),dp_c_dt); hold on
+% % dp_c_dt = smooth(dp_c_dt,50);
+% % plot(t(1:end-1),dp_c_dt)
+% 
+% cut = 200;
+% 
+% C1_set = (1./dp_c_dt(1:end-cut+1)) .* ( q_bpp(1:end-cut) - q_c(1:end-cut) );
+% 
+% t = 1:801;
+% % % Plotting
+% figure(2);clf(2)
+% plot(t, C1_set)
+% 
+% % Avereging out noise
+% C1 = sum(C1_set(500:800))/(800-500);
+%  
 
 
 
--- oppg/Files 2015/Simulation_Script.m
+++ oppg/Files 2015/Simulation_Script.m
@@ -2,24 +2,45 @@
 % Simulation_Script
 % do not run before estimates from Estimation Script has been calculated
 
-
+tic
 
 % Choose LOG input
-LOG = LOG1;
+LOG = LOG3;
+t1 = 1; % LOG3: 500 ;
+t2 = length(LOG.t)-1000; % LOG3: 5000 ;
 
 % Assigning variables
-time = length(LOG.t);
-t = LOG.t;
-p_c_ds = LOG.p_c_ds;
-q_p = LOG.q_p;
-q_bpp = LOG.q_bpp;
-p_p = LOG.p_p;
-z_c = LOG.z_c;
+t = LOG.t(t1:t2);
+time = length(t);
+p_c_ds = LOG.p_c_ds(t1:t2);
+q_p = LOG.q_p(t1:t2);
+q_bpp = LOG.q_bpp(t1:t2);
+z_c = LOG.z_c(t1:t2);
+
+% Parameters for estimation
+M = 1e+9;
+
+disp(theta)
+disp(C1)
+disp(C2)
 
 % Simulation
 sim SystemSim
 
 % Verification
+toc
 
-
-
+%plotting
+if 0
+    
+    figure(1); clf(1)
+    plot(q_out.time,q_out.signals.values)
+    figure(2); clf(2)
+    plot(q_c_out.time,q_c_out.signals.values)
+    figure(3); clf(3)
+    plot(p_c_out.time,p_c_out.signals.values)
+    figure(4); clf(4)
+    plot(p_p_out.time,p_p_out.signals.values)
+    
+    
+end
